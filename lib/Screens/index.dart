072e5d5712b410b3751a05100d4471c90ea9dd8e
Lab_Iot <gustavoguguqueiroz@hotmail.com>
2022-07-18 16:42:32 -0300

    18.7

--- lib/Screens/index.dart
+++ lib/Screens/index.dart
@@ -40,84 +40,45 @@ class IndexColumnState extends State<IndexColumn> {
             child: ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const IndexScreen(),
-                    ),
-                  );
-                },
+                  MaterialPageRoute(
+                    builder: (context) => const IndexScreen(),
+                  ),
+                );
+              },
               child: const Text('Cadastrar novo dispositivo'),
             ),
           ),
-          // GridView.builder(
-          //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          //     crossAxisCount: 1,
-          //     mainAxisExtent: 180,
-          //   ),
-          //   itemCount: vehicles.length,
-          //   itemBuilder: (context, index) {
-          //     return _FeatureItem(vehicles[index].plate, vehicles[index].description, onClick: () {
-          //       _vehicleInfo(context, vehicles[index]);
-          //     },);
-          //   },
-          // ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              color: Theme.of(context).primaryColorDark,
+              child: InkWell(
+                onTap: () => const IndexScreen(), // todo: solicitar servidor
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text('Tranca',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            letterSpacing: 3.0,
+                          )),
+                      Text('Código321',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-// void _DeviceInfo(BuildContext context, Vehicle vehicle) {
-//   Navigator.of(context).push(
-//     MaterialPageRoute(
-//       builder: (context) => VehicleInfo(vehicle),
-//     ),
-//   );
-// }
-//
-// class _FeatureItem extends StatelessWidget {
-//   final String plate;
-//   final String description;
-//   final Function onClick;
-//
-//   _FeatureItem(this.plate, this.description, {required this.onClick});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.all(8.0),
-//       child: Material(
-//         color: Theme.of(context).primaryColor,
-//         child: InkWell(
-//           onTap: () => onClick(),
-//           child: Container(
-//             padding: EdgeInsets.all(16.0),
-//             height: 60,
-//             width: 110,
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: <Widget>[
-//                 Text('Placa:',
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontSize: 16,
-//                     )),
-//                 Text(plate,
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontSize: 40,
-//                       letterSpacing: 3.0,
-//                     )),
-//                 Text('Descrição: ' + description,
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontSize: 14,
-//                     )),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
